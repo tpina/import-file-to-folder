d6ac2640119b608fee90253b9c7853dc3e8ade1d
Tiago Pina <tpina@marketfactory.com>
2019-01-28 17:21:00 -0500

    Removed unused stuff

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -9,7 +9,6 @@ import * as assert from "assert";
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 // import * as vscode from 'vscode';
-import * as importFileToFolder from "../extension";
 
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Extension import file to folder Tests", function() {
